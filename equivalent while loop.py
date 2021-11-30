bb4f0e0102340ccc1701bb4eb468f98c5091ae1e
Ihimbru-K <87714194+Ihimbru-K@users.noreply.github.com>
2021-11-30 18:16:26 +0100

    Add files via upload

---
+++ equivalent while loop.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 30 10:00:23 2021
+
+@author: mon pc
+"""
+
+
+print('my name is')
+i = 0
+while i < 5:
+    print(" Kanyimi five times ")
+    i = i + 1
